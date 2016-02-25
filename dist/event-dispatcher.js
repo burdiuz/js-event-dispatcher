5d1753d9248280db9d32d287b5cfc57da52010d6
Oleg Galaburda <burdiuz@gmail.com>
2016-02-25 10:49:13 +0200

    update distribution files

--- dist/event-dispatcher.js
+++ dist/event-dispatcher.js
@@ -125,12 +125,12 @@
        * 3. In this method create function StoppableEvent that will extend from this event and add these functions,
        *    then instantiate it for this one cycle.
        */
-       event.stopPropagation = stopPropagation;
-       event.stopImmediatePropagation = stopImmediatePropagation;
+      event.stopPropagation = stopPropagation;
+      event.stopImmediatePropagation = stopImmediatePropagation;
       /*
-      var rmStopPropagation = EventDispatcher.setupOptional(event, 'stopPropagation', stopPropagation);
-      var rmStopImmediatePropagation = EventDispatcher.setupOptional(event, 'stopImmediatePropagation', stopImmediatePropagation);
-      */
+       var rmStopPropagation = EventDispatcher.setupOptional(event, 'stopPropagation', stopPropagation);
+       var rmStopImmediatePropagation = EventDispatcher.setupOptional(event, 'stopImmediatePropagation', stopImmediatePropagation);
+       */
       var priorities = getHashByKey(event.type, this._listeners);
       if (priorities) {
         var list = Object.getOwnPropertyNames(priorities).sort(function(a, b) {
@@ -151,9 +151,9 @@
       delete event.stopPropagation;
       delete event.stopImmediatePropagation;
       /*
-      rmStopPropagation();
-      rmStopImmediatePropagation();
-      */
+       rmStopPropagation();
+       rmStopImmediatePropagation();
+       */
     }
   
     function createList(eventType, priority, target) {
@@ -191,8 +191,12 @@
   
     return EventListeners;
   })();
-  
-  function EventDispatcher() {
+  /**
+   *
+   * @param eventPreprocessor {?Function}
+   * @constructor
+   */
+  function EventDispatcher(eventPreprocessor) {
     /**
      * @type {EventListeners}
      */
@@ -215,7 +219,11 @@
     }
   
     function dispatchEvent(event, data) {
-      _listeners.call(EventDispatcher.getEvent(event, data));
+      var eventObject = EventDispatcher.getEvent(event, data);
+      if (eventPreprocessor) {
+        eventObject = eventPreprocessor.call(this, eventObject);
+      }
+      _listeners.call(eventObject);
     }
   
     this.addEventListener = addEventListener;
@@ -234,21 +242,21 @@
   }
   
   /*
-  function setupOptional(target, name, value) {
-    var cleaner = null;
-    if (name in target) {
-      cleaner = function() {
-      };
-    } else {
-      target[name] = value;
-      cleaner = function() {
-        delete target[name];
-      };
-    }
-    return cleaner;
-  }
-  EventDispatcher.setupOptional = setupOptional;
-  */
+   function setupOptional(target, name, value) {
+   var cleaner = null;
+   if (name in target) {
+   cleaner = function() {
+   };
+   } else {
+   target[name] = value;
+   cleaner = function() {
+   delete target[name];
+   };
+   }
+   return cleaner;
+   }
+   EventDispatcher.setupOptional = setupOptional;
+   */
   
   EventDispatcher.isObject = isObject;
   
